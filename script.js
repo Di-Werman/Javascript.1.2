f395380b58b19016c6d3fa2d61b96f433781aaf2
Di-Werman <118116167+Di-Werman@users.noreply.github.com>
2023-02-18 20:20:09 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// Test
+/*
+Number
+String
+Boolean
+Object
+Nul
+Undefined
+*/
+
+var num = 42
+var string = 'Message'
+var isTrue = true//false
+var obj = {a: 1}
+var nothing = null
+var undef = undefined
+
+console.log(typeof nothing)
